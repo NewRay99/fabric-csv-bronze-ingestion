74c471fe1bfde4acdc56b151cbe2e4de956b48eb
NewRay99 <98088505+NewRay99@users.noreply.github.com>
2026-09-16 13:32:23 +0100

    Update 01a_cfg_schema_capture_live.py

--- project X/01a_cfg_schema_capture_live.py
+++ project X/01a_cfg_schema_capture_live.py
@@ -4,7 +4,6 @@
 
 # META {
 # META   "kernel_info": {
-# META     "jupyter_kernel_name": null,
 # META     "name": "synapse_pyspark"
 # META   },
 # META   "dependencies": {
@@ -18,17 +17,7 @@
 # META         }
 # META       ]
 # META     }
-# META   },
-# META   "spark_compute": {
-# META     "compute_id": "/trident/default",
-# META     "session_options": {
-# META       "conf": {
-# META         "spark.synapse.nbs.session.timeout": "1200000"
-# META       }
-# META     }
-# META   },
-# META   "sessionKeepAliveTimeout": 0,
-# META   "a365ComputeOptions": null
+# META   }
 # META }
 
 # MARKDOWN ********************
