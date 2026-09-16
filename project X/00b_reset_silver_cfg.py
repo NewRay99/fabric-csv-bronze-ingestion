8c459625b65333b15713bb8b5d11d796cbba62fc
NewRay99 <98088505+NewRay99@users.noreply.github.com>
2026-09-16 13:31:12 +0100

    Update 00b_reset_silver_cfg.py

--- project X/00b_reset_silver_cfg.py
+++ project X/00b_reset_silver_cfg.py
@@ -7,32 +7,24 @@
 # META     "name": "synapse_pyspark"
 # META   },
 # META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "d286fa39-f255-4ba7-a982-32cb69362ef7",
+# META       "default_lakehouse_name": "LH_BCT_WMPP",
+# META       "default_lakehouse_workspace_id": "fefdb483-d26c-4bd9-9a4f-0c41cc786770",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "d286fa39-f255-4ba7-a982-32cb69362ef7"
+# META         }
+# META       ]
+# META     },
 # META     "warehouse": {
+# META       "default_warehouse": "9a24911d-3f57-4c68-889f-88b1dbbc2cc0",
 # META       "known_warehouses": [
 # META         {
 # META           "id": "9a24911d-3f57-4c68-889f-88b1dbbc2cc0",
 # META           "type": "Lakewarehouse"
 # META         }
-# META       ],
-# META       "default_warehouse": "9a24911d-3f57-4c68-889f-88b1dbbc2cc0"
-# META     },
-# META     "lakehouse": {
-# META       "known_lakehouses": [
-# META         {
-# META           "id": "d286fa39-f255-4ba7-a982-32cb69362ef7"
-# META         }
-# META       ],
-# META       "default_lakehouse": "d286fa39-f255-4ba7-a982-32cb69362ef7",
-# META       "default_lakehouse_name": "LH_BCT_WMPP",
-# META       "default_lakehouse_workspace_id": "fefdb483-d26c-4bd9-9a4f-0c41cc786770"
-# META     }
-# META   },
-# META   "spark_compute": {
-# META     "compute_id": "/trident/default",
-# META     "session_options": {
-# META       "conf": {
-# META         "spark.synapse.nbs.session.timeout": "1200000"
-# META       }
+# META       ]
 # META     }
 # META   }
 # META }
